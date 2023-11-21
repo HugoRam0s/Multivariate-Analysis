4d71e86b631017ad8beacfbc664ac3edd7f7bdef
HugoRam0s <hugo.francisco.santos.ramos@tecnico.ulisboa.pt>
2023-11-21 14:32:16 +0000

    Update InitialAnalysis_Hugo.R
    Alterações para o dataset pré-processado pela Carolina

--- code/InitialAnalysis_Hugo.R
+++ code/InitialAnalysis_Hugo.R
@@ -9,7 +9,7 @@ library(mvtnorm)
 library(car)
 
 ## Read dataset
-water <- read_csv('C:/Users/hugof/OneDrive/Documentos/GitHub/Multivariate-Analysis/data/raw/water_potability.csv',show_col_types = FALSE)
+water <- read_csv('C:/Users/hugof/OneDrive/Documentos/GitHub/Multivariate-Analysis/data/processed/cleaned_data.csv',show_col_types = FALSE)
 
 ## Preprocessing
 # Check how much percentage of data is missing
@@ -18,47 +18,47 @@ Na_ph <- (nrow(water [!complete.cases(water$ph), ])/3276)*100
 Na_Sulphate <- (nrow(water [!complete.cases(water$Sulfate), ])/3276)*100 
 Na_Trihalo <- (nrow(water [!complete.cases(water$Trihalomethanes), ])/3276)*100
 # Fill Na values with the median of the respective variable
-water1 <- na.aggregate(water)
-water <- water1
-write.csv(water, "C:/Users/hugof/OneDrive/Documentos/GitHub/Multivariate-Analysis/data/processed/waterProcessed.csv", row.names=TRUE)
+#water1 <- na.aggregate(water)
+#water <- water1
+#write.csv(water, "C:/Users/hugof/OneDrive/Documentos/GitHub/Multivariate-Analysis/data/processed/waterProcessed.csv", row.names=TRUE)
 
 ## Basic statistic analysis in tables
 
-summary(water[,1:9])
-describe(water[,1:9])
-round(var(water[,1:9]),3)
-round(cov(water[,1:9]),3)
+summary(water[,2:10])
+describe(water[,2:10])
+round(var(water[,2:10]),3)
+round(cov(water[,2:10]),3)
 round(cor(water),3)
 
 ## Data visualization
 
-for(i in names(water[,1:9])){
+for(i in names(water[,2:10])){
   hist(water[[i]], main = i, xlab = names(water)[i],breaks=50)
 }
 
-pairs.panels(water[,1:9], smooth = FALSE, scale = FALSE, density=TRUE,
+pairs.panels(water[,2:10], smooth = FALSE, scale = FALSE, density=TRUE,
              ellipses=FALSE,digits = 2,hist.col="green")
 
-ggpairs(water[,1:9], title="Correlogram")
+ggpairs(water[,2:10], title="Correlogram")
 
 water$Potability<-as.factor(water$Potability)
-ggpairs(water[,1:9], ggplot2::aes(colour=water$Potability))
+ggpairs(water[,2:10], ggplot2::aes(colour=water$Potability))
 
 ## Checking for normality 
 
 #Comparison between real data and random generated normal multivariate observations
 n<-10000
 set.seed(1738)
-teoric<-rmvnorm(n,colMeans(water[,1:9]), cov(water[,1:9]))
-colnames(teoric)<-colnames(water[,1:9])
+teoric<-rmvnorm(n,colMeans(water[,2:10]), cov(water[,2:10]))
+colnames(teoric)<-colnames(water[,2:10])
 
 print(round(cov(teoric),3))###Sample covariance matrix
-print(round(cov(water[,1:9]),3)) ###Theoretical covariance matrix
-round(cor(water[,1:9])-cor(teoric),3)
+print(round(cov(water[,2:10]),3)) ###Theoretical covariance matrix
+round(cor(water[,2:10])-cor(teoric),3)
 
 print(round(cor(teoric),3))###Sample correlation matrix
-print(round(cor(water[,1:9]),3)) ###Theoretical correlation matrix
-round(cor(water[,1:9])-cor(teoric),3)
+print(round(cor(water[,2:10]),3)) ###Theoretical correlation matrix
+round(cor(water[,2:10])-cor(teoric),3)
 
 ## Using quantile-quantile plots and mahalanobis distance, multivariate normality tests
 
@@ -83,19 +83,19 @@ qqnorm(water$Turbidity)
 qqline(water$Turbidity)
 
 #Quantile plots of mahalanobis distance
-d<-mahalanobis(water[,1:9], colMeans(water[,1:9]), cov(water[,1:9]))
+d<-mahalanobis(water[,2:10], colMeans(water[,2:10]), cov(water[,2:10]))
 d<-sort(d)
-qqPlot(d,distribution = "chisq",df=ncol(water[,1:9]),main="Quantile-quantile plot of mahalanobis distance",pch=20,col="red")
+qqPlot(d,distribution = "chisq",df=ncol(water[,2:10]),main="Quantile-quantile plot of mahalanobis distance",pch=20,col="red")
 
 
 #Mardia's Multivariate Normality test
-mvn(data = water[,1:9], mvnTest = "mardia")
+mvn(data = water[,2:10], mvnTest = "mardia")
 
 #Henze-Zirkler Multivariate Normality test
-mvn(data = water[,1:9], mvnTest = "hz")
+mvn(data = water[,2:10], mvnTest = "hz")
 
 #Doornik-Hansen Multivariate Normality test
-mvn(data = water[,1:9], mvnTest = "dh")
+mvn(data = water[,2:10], mvnTest = "dh")
 
 # Energy test
-mvnorm.etest(water[,1:9], R=100)
+mvnorm.etest(water[,2:10], R=100)
